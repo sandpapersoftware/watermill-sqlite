65fc81dcdde5ca69efa40cab5bd36342730f0083
pbedat <pbedat@sandpaperstoftware.de>
2025-10-30 09:38:14 +0100

    adds notifier

--- wmsqlitemodernc/subscription.go
+++ wmsqlitemodernc/subscription.go
@@ -15,9 +15,12 @@ import (
 type subscription struct {
 	DB           SQLiteDatabase
 	pollTicker   *time.Ticker
+	pollInterval time.Duration
 	lockTicker   *time.Ticker
 	lockDuration time.Duration
 	nackChannel  func() <-chan time.Time
+	notifierC    <-chan struct{}
+	topic        string
 
 	sqlLockConsumerGroup   string
 	sqlExtendLock          string
@@ -82,7 +85,15 @@ func (s *subscription) NextBatch(ctx context.Context) (batch []rawMessage, err e
 	if err != nil {
 		return nil, fmt.Errorf("unable to query next message batch: %w", err)
 	}
-	return buildBatch(rows)
+
+	batch, err = buildBatch(rows)
+	if err != nil {
+		return nil, fmt.Errorf("unable to build message batch: %w", err)
+	}
+	if len(batch) == 0 {
+		return nil, tx.Rollback()
+	}
+	return batch, nil
 }
 
 func buildBatch(rows *sql.Rows) (batch []rawMessage, err error) {
@@ -183,13 +194,16 @@ func (s *subscription) Run(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case <-s.pollTicker.C:
+		case <-s.notifierC:
+			s.pollTicker.Reset(s.pollInterval)
 		}
 
 		batch, err = s.NextBatch(ctx)
 		if err != nil {
 			if !errors.Is(err, context.Canceled) {
-				s.logger.Error("next message batch query failed", err, nil)
+				return
 			}
+			s.logger.Error("next message batch query failed", err, nil)
 			continue
 		}
 		if len(batch) == 0 {
@@ -199,16 +213,18 @@ func (s *subscription) Run(ctx context.Context) {
 		for _, next := range batch {
 			if err = s.Send(ctx, next); err != nil {
 				if !errors.Is(err, context.Canceled) {
-					s.logger.Error("failed to process queued message", err, nil)
+					return
 				}
+				s.logger.Error("failed to process queued message", err, nil)
 				continue
 			}
 		}
 
 		if err = s.ReleaseLock(ctx); err != nil {
 			if !errors.Is(err, context.Canceled) {
-				s.logger.Error("failed to acknowledge processed messages", err, nil)
+				return
 			}
+			s.logger.Error("failed to acknowledge processed messages", err, nil)
 		}
 	}
 }
